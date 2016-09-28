40022840e2a89b574c22269aa86b8cf2205d74f4
beburkart <lyenn_@Solynn.local>
2016-09-28 16:05:46 +0200

    Datei route.php

---
+++ app/route.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html>
+<head>
+    
+    <!-- Links -->
+    <link href="style/style.css" rel="stylesheet" type="text/css">
+
+    <meta charset="utf-8">
+    <title>waythere stories - route</title>
+    
+</head>
+
+<body>
+    
+    <h1> Routenübersicht </h1>
+    
+</body>
+</html>
